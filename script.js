26c4d78253f5ea39a76d8b4690fb39141f269f86
ewinITservices <143334809+ewinITservices@users.noreply.github.com>
2023-09-01 19:13:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,8 @@
 function openStatusPage() {
   window.open('http://three.mypisowifi.net', '_blank');
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    var audio = document.getElementById("myAudio");
+    audio.play();
+});
